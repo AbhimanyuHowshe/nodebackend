12673889a2cdbf9332813b9c6198e875855cc058
Abhimanyu Howshe <abhi.howshe@gmail.com>
2024-01-06 13:47:21 +0530

    Util code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse{
+    constructor(statuscode, message = "Success",data ){
+        this.message = message;
+        this.statuscode = statuscode;
+        this.data = data;
+        this.success = success < 400;
+    }
+
+}
